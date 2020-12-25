f773f0d2de77237a8cda4f017d739e941f9814a3
Bello Olakitan <bello.olakitan@gmail.com>
2020-12-24 23:05:38 -0500

    enables password hashing

--- Models/User.js
+++ Models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bycrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -12,6 +13,27 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+userSchema.pre('save', async function(next) {
+    try {
+        const salt = await bycrypt.genSalt(10);
+        const hashedPassword = await bycrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+        console.log(this.email, this.password)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// userSchema.post('save', async function(next) {
+//     try {
+//         console.log('called after saving');
+//     } catch (error) {
+//         next(error)
+//     }
+// })
 
 const User = mongoose.model('user', userSchema)
 
