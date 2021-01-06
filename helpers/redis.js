a18552d880c5baff859a0cc9731cf13c2dea8882
Bello Olakitan <bello.olakitan@gmail.com>
2021-01-06 14:38:35 -0500

    redis credentials to env variables

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,9 +1,10 @@
 const redis = require('redis')
+require('dotenv').config()
 
 const client = redis.createClient({
-    port: 16109,
-    host: 'ec2-174-129-249-71.compute-1.amazonaws.com',
-    password: 'p810ab01af0039611af085e24e0bc21b3a24a57bd1fc0f521782197a75d3f4f27'
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD
 })
 
 client.on('connect', () => {
