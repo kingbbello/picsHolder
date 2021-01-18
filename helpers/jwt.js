2e8bf9d04910703fcbbc44db224ccd11c698f89c
kingbbello <36551677+kingbbello@users.noreply.github.com>
2021-01-18 11:11:09 -0500

    Updated jwt helper. Removed issuer

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -7,7 +7,7 @@ module.exports = {
         return new Promise((res, rej) => {
             const payload = {}
             const secret = process.env.ACCESS_TOKEN_SECRET
-            const options = { expiresIn: "1h", issuer: "pickurpage.com", audience: userId }
+            const options = { expiresIn: "1h", audience: userId }
 
             JWT.sign(payload, secret, options, (err, token) => {
                 if (err) {
